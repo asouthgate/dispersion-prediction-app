bc8cf19bb23e5d81ddaa1fc014c0f0d9b72b9c9c
Jeffrey Morgan <42075230+DrJeffreyMorgan@users.noreply.github.com>
2021-03-04 11:27:19 +0000

    Tidy.

--- circuitscape_app/bats.R
+++ circuitscape_app/bats.R
@@ -9,40 +9,28 @@ options(shiny.port=8100)
 ui <- fluidPage(
     
     titlePanel("Bats"),
+
     sidebarLayout(
         sidebarPanel(
             fluidRow(
-                column(
-                    6,
-                    textInput(
-                        inputId="easting",
-                        label="Easting:",
-                        value="274257"
-                    )
-                ),
-                column(
-                    6,
-                    textInput(
-                        inputId="northing",
-                        label="Northing:",
-                        value="66207"
-                    )
-                )
+                column(6, textInput(inputId="easting", label="Easting:", value="274257")),
+                column(6, textInput(inputId="northing", label="Northing:", value="66207"))
             ),
             fluidRow(
-                column(
-                    12,
-                    verbatimTextOutput(outputId="coordinate")
-                )
+                column(12, h5("Coordinate")),
+                column(12, verbatimTextOutput(outputId="coordinate"))
             ),
-            sliderInput(
-                inputId="radius",
-                label="Radius:",
-                min=1,
-                max=1000,
-                value=300
-            )
+            fluidRow(
+                column(12, h5("Clicked 27700")),
+                column(12, verbatimTextOutput(outputId="clicked27700")),
+            ),
+            fluidRow(
+                column(12, h5("Clicked 4326")),
+                column(12, verbatimTextOutput(outputId="clicked4326")),
+            ),
+            sliderInput(inputId="radius", label="Radius:", min=1, max=1000, value=300)
         ),
+        
         mainPanel(
             fillPage(leafletOutput("map"))
         )
@@ -53,6 +41,8 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     convertPoint <- function(x, y, sourceCRS, destinationCRS) {
+        x = as.numeric(x)
+        y = as.numeric(y)
         sourcePoint = st_point(c(x, y))
         sfc = st_sfc(sourcePoint, crs=sourceCRS)
         destinationPoint = st_transform(sfc, destinationCRS)
@@ -60,27 +50,51 @@ server <- function(input, output) {
         return(coordinates)
     }
 
-    coord <- reactive({
-        x = as.numeric(input$easting)
-        y = as.numeric(input$northing)
-        c = convertPoint(x, y, 27700, 4326)
-        return(c)
-    })
+    coord <- reactive(convertPoint(input$easting, input$northing, 27700, 4326))
 
     map <- reactive({
         leaflet() %>%
             addTiles() %>%
-            setView(lng=coord()[1], lat=coord()[2], zoom=8)
+            setView(lng=coord()[1], lat=coord()[2], zoom=12)
     })
+
+    output$map <- renderLeaflet(map())
 
     output$coordinate <- renderText({
         paste(coord()[1], coord()[2], sep=",")
     })
 
-    output$map <- renderLeaflet(map())
-
     observeEvent(coord(), {
         print(coord())
+    })
+
+    observeEvent(input$radius, {
+        print(input$radius)
+    })
+
+    observe({
+        mapClick <- input$map_click
+        if (is.null(mapClick)) return()
+        print(paste("Latitude: ", mapClick$lat, "Longtitude: ", mapClick$lng))
+        addMarkers(
+            map(),
+            mapClick$lng,
+            mapClick$lat
+        )
+    })
+
+    output$clicked27700 <- renderText({
+        mapClick <- input$map_click
+        if (is.null(mapClick)) return()
+        bngCoord <- convertPoint(mapClick$lng, mapClick$lat, 4326, 27700)
+        x = bngCoord[1]
+        y = bngCoord[2]
+        paste(x, y, sep=",")
+    })
+
+    output$clicked4326 <- renderText({
+        mapClick <- input$map_click
+        paste(mapClick$lng, mapClick$lat, sep=",")
     })
 
 }
