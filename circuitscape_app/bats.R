87caa161e9cb61b66dfc069f73e97526c59a0154
Jeffrey Morgan <42075230+DrJeffreyMorgan@users.noreply.github.com>
2021-03-04 15:40:46 +0000

    Make coordinates reactive.

--- circuitscape_app/bats.R
+++ circuitscape_app/bats.R
@@ -12,25 +12,20 @@ ui <- fluidPage(
 
     sidebarLayout(
         sidebarPanel(
+            h3("Roost Coordinates"),
             fluidRow(
-                column(6, textInput(inputId="easting", label="Easting:", value="274257")),
-                column(6, textInput(inputId="northing", label="Northing:", value="66207"))
-            ),
-            fluidRow(
-                column(12, h5("Coordinate")),
-                column(12, verbatimTextOutput(outputId="coordinate"))
-            ),
-            fluidRow(
-                column(12, h5("Clicked 27700")),
+                column(12, strong(p("EPSG:27700"))),
                 column(12, verbatimTextOutput(outputId="clicked27700")),
             ),
             fluidRow(
-                column(12, h5("Clicked 4326")),
+                column(12, strong(p("EPSG:4326"))),
                 column(12, verbatimTextOutput(outputId="clicked4326")),
             ),
-            sliderInput(inputId="radius", label="Radius in meters:", min=1, max=1000, value=300),
+            
+            h3("Distance from Roost"),
+            sliderInput(inputId="radius", label="Radius in meters:", min=100, max=1000, value=300),
 
-            # File Upload
+            h3("Street Lighting"),
             fileInput("file", NULL, buttonLabel = "Upload CSV", accept=c(".csv"),  multiple=TRUE),
             tableOutput("files"),
             numericInput("n", "Rows", value=5, min=1, step=1),
@@ -46,29 +41,27 @@ ui <- fluidPage(
 
 server <- function(input, output) {
 
-    convertPoint <- function(x, y, sourceCRS, destinationCRS) {
+    create_st_point <- function(x, y) {
         x = as.numeric(x)
         y = as.numeric(y)
-        sourcePoint = st_point(c(x, y))
+        st_point(c(x, y))
+    }
+
+    convertPoint <- function(x, y, sourceCRS, destinationCRS) {
+        sourcePoint = create_st_point(x, y)
         sfc = st_sfc(sourcePoint, crs=sourceCRS)
         destinationPoint = st_transform(sfc, destinationCRS)
         coordinates = st_coordinates(destinationPoint)
         return(coordinates)
     }
 
-    coord <- reactive(convertPoint(input$easting, input$northing, 27700, 4326))
-
     map <- reactive({
         leaflet() %>%
             addTiles() %>%
-            setView(lng=coord()[1], lat=coord()[2], zoom=14)
+            setView(lng=-3.964, lat=50.494, zoom=13)
     })
 
     output$map <- renderLeaflet(map())
-
-    output$coordinate <- renderText({
-        paste(coord()[1], coord()[2], sep=",")
-    })
 
     observeEvent(input$radius, {
         print(paste("Radius:", input$radius))
@@ -84,19 +77,28 @@ server <- function(input, output) {
             addCircles(lng=mapClick$lng, lat=mapClick$lat, weight = 1, radius=as.numeric(input$radius))
     })
 
-    output$clicked27700 <- renderText({
+    clicked27700 <- reactive({
         mapClick <- input$map_click
         if (is.null(mapClick)) return()
-        bngCoord <- convertPoint(mapClick$lng, mapClick$lat, 4326, 27700)
-        x = bngCoord[1]
-        y = bngCoord[2]
+        convertPoint(mapClick$lng, mapClick$lat, 4326, 27700)
+    })
+
+    clicked4326 <- reactive({
+        mapClick <- input$map_click
+        if (is.null(mapClick)) return()
+        create_st_point(mapClick$lng, mapClick$lat)
+    })
+
+    output$clicked27700 <- renderText({
+        x = clicked27700()[1]
+        y = clicked27700()[2]
         paste(x, y, sep=",")
     })
 
     output$clicked4326 <- renderText({
-        mapClick <- input$map_click
-        if (is.null(mapClick)) return()
-        paste(mapClick$lng, mapClick$lat, sep=",")
+        x = clicked4326()[1]
+        y = clicked4326()[2]
+        paste(x, y, sep=",")
     })
 
 
@@ -116,6 +118,8 @@ server <- function(input, output) {
     })
 
     output$head <- renderTable({
+        print(paste("datapath:", input$file$datapath))
+        print(paste("type:", input$file$type))
         head(data(), input$n)
     })
 
