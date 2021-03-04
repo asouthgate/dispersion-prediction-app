0a6f09170a5a6c0f8328e04bfcfa9d3fec618349
Jeffrey Morgan <42075230+DrJeffreyMorgan@users.noreply.github.com>
2021-03-04 16:18:30 +0000

    Add comments and refactor.

--- circuitscape_app/bats.R
+++ circuitscape_app/bats.R
@@ -49,11 +49,7 @@ ui <- fluidPage(
 
 server <- function(input, output) {
 
-    create_st_point <- function(x, y) {
-        x = as.numeric(x)
-        y = as.numeric(y)
-        st_point(c(x, y))
-    }
+    create_st_point <- function(x, y) { st_point(c(as.numeric(x), as.numeric(y))) }
 
     convertPoint <- function(x, y, sourceCRS, destinationCRS) {
         sourcePoint = create_st_point(x, y)
@@ -63,6 +59,14 @@ server <- function(input, output) {
         return(coordinates)
     }
 
+    # Get the x coordinate of a reactive st_point
+    x <- function(point) { point()[1] }
+
+    # Get the y coordinate of a reactive st_point
+    y <- function(point) { point()[2] }
+    
+    formatCoordinate <- function(n) { format(n, digits=3, nsmall=3) }
+
     map <- reactive({
         leaflet() %>%
             addTiles() %>%
@@ -71,10 +75,7 @@ server <- function(input, output) {
 
     output$map <- renderLeaflet(map())
 
-    observeEvent(input$radius, {
-        print(paste("Radius:", input$radius))
-    })
-
+    # Add/update map marker and circle at the clicked map point
     observe({
         mapClick <- input$map_click
         if (is.null(mapClick)) return()
@@ -85,46 +86,30 @@ server <- function(input, output) {
             addCircles(lng=mapClick$lng, lat=mapClick$lat, weight = 1, radius=as.numeric(input$radius))
     })
 
+    # Get the coordinate of the clicked map point in EPSG:27700 (BNG)
     clicked27700 <- reactive({
         mapClick <- input$map_click
         if (is.null(mapClick)) return()
         convertPoint(mapClick$lng, mapClick$lat, 4326, 27700)
     })
 
+    # Get the coordinate of the clicked map point in EPSG:4326 (WSG84)
     clicked4326 <- reactive({
         mapClick <- input$map_click
         if (is.null(mapClick)) return()
         create_st_point(mapClick$lng, mapClick$lat)
     })
 
-    output$easting <- renderText({
-        x = clicked27700()[1]
-        paste(x)
-    })
-
-    output$northing <- renderText({
-        y = clicked27700()[2]
-        paste(y)
-    })
-
-    output$longitude <- renderText({
-        x = clicked4326()[1]
-        paste(x)
-    })
-
-    output$latitude <- renderText({
-        y = clicked4326()[2]
-        paste(y)
-    })
+    # Roost Coordinates
+    output$easting <- renderText(formatCoordinate(x(clicked27700)))
+    output$northing <- renderText(formatCoordinate(y(clicked27700)))
+    output$longitude <- renderText(formatCoordinate(x(clicked4326)))
+    output$latitude <- renderText(formatCoordinate(y(clicked4326)))
 
 
     # File Upload
-
-    output$files <- renderTable(input$upload)
-
     data <- reactive({
         req(input$file)
-
         ext <- tools::file_ext(input$file$name)
         switch(
             ext,
@@ -132,7 +117,6 @@ server <- function(input, output) {
             validate("Invalid file; Please upload a .csv file")
         )
     })
-
     output$head <- renderTable({
         print(paste("datapath:", input$file$datapath))
         print(paste("type:", input$file$type))
