e66bb7c53b057a50c7ba032929bc69ada50b1ebf
Jeffrey Morgan <42075230+DrJeffreyMorgan@users.noreply.github.com>
2021-03-04 11:47:04 +0000

    Add file upload.

--- circuitscape_app/bats.R
+++ circuitscape_app/bats.R
@@ -28,7 +28,13 @@ ui <- fluidPage(
                 column(12, h5("Clicked 4326")),
                 column(12, verbatimTextOutput(outputId="clicked4326")),
             ),
-            sliderInput(inputId="radius", label="Radius:", min=1, max=1000, value=300)
+            sliderInput(inputId="radius", label="Radius:", min=1, max=1000, value=300),
+
+            # File Upload
+            fileInput("file", NULL, buttonLabel = "Upload CSV", accept=c(".csv"),  multiple=TRUE),
+            tableOutput("files"),
+            numericInput("n", "Rows", value=5, min=1, step=1),
+            tableOutput("head")
         ),
         
         mainPanel(
@@ -88,6 +94,26 @@ server <- function(input, output) {
         mapClick <- input$map_click
         if (is.null(mapClick)) return()
         paste(mapClick$lng, mapClick$lat, sep=",")
+    })
+
+
+    # File Upload
+
+    output$files <- renderTable(input$upload)
+
+    data <- reactive({
+        req(input$file)
+
+        ext <- tools::file_ext(input$file$name)
+        switch(
+            ext,
+            csv = vroom::vroom(input$file$datapath, delim = ","),
+            validate("Invalid file; Please upload a .csv file")
+        )
+    })
+
+    output$head <- renderTable({
+        head(data(), input$n)
     })
 
 }
