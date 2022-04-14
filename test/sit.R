92508740238f4c5e64050ce4871a7b9fb0ec2874
j <joelsouthgate@googlemail.com>
2022-04-14 13:13:32 +0100

    allow db specification in config

--- test/sit.R
+++ test/sit.R
@@ -5,14 +5,19 @@ library(rpostgis)
 
 source("circuitscape_app/algorithm_parameters.R")
 source("circuitscape_app/db.R")
+source("circuitscape_app/transform.R")
 
 test_that("Vector databases are available", {
 
     config <- configr::read.config("~/.bats.cfg")
+    database_host <- config$database$host
     database_name <- config$database$name
     database_password <- config$database$password
     database_user <- config$database$user
     database_port <- config$database$port
+    roads_table <- gsub("'", "", config$database$roads_table)
+    rivers_table <- gsub("'", "", config$database$rivers_table)
+    buildings_table <- gsub("'", "", config$database$buildings_table)
 
     algorithm_parameters <- AlgorithmParameters$new(
         Roost$new(397093.199, 87698.191, 3000),
@@ -23,12 +28,12 @@ test_that("Vector databases are available", {
         LampResistance$new(resmax=10, xmax=5, ext=100)
     )
 
-    ext <- create_extent(algorithm_parameters)
+    ext <- create_extent(algorithm_parameters$roost$x, algorithm_parameters$roost$y, algorithm_parameters$roost$radius)
 
     suppressWarnings({
-        x <- read_db_vector("watercourselink", ext, database_host, database_name, database_port, database_user, database_password)
-        y <- read_db_vector("roadlink", ext, database_host, database_name, database_port, database_user, database_password)
-        z <- read_db_vector("building", ext, database_host, database_name, database_port, database_user, database_password)
+        x <- read_db_vector(rivers_table, ext, database_host, database_name, database_port, database_user, database_password)
+        y <- read_db_vector(roads_table, ext, database_host, database_name, database_port, database_user, database_password)
+        z <- read_db_vector(buildings_table, ext, database_host, database_name, database_port, database_user, database_password)
 
         expect_gt(length(x), 0)
         expect_gt(length(y), 0)
@@ -42,8 +47,12 @@ test_that("Raster databases are available", {
     config <- configr::read.config("~/.bats.cfg")
     database_name <- config$database$name
     database_password <- config$database$password
+    database_host <- config$database$host
     database_user <- config$database$user
     database_port <- config$database$port
+    dtm_table <- gsub("'", "", config$database$dtm_table)
+    dsm_table <- gsub("'", "", config$database$dsm_table)
+    lcm_table <- gsub("'", "", config$database$lcm_table)
 
     algorithm_parameters <- AlgorithmParameters$new(
         Roost$new(397093.199, 87698.191, 3000),
@@ -54,12 +63,12 @@ test_that("Raster databases are available", {
         LampResistance$new(resmax=10, xmax=5, ext=100)
     )
 
-    ext <- create_extent(algorithm_parameters)
+    ext <- create_extent(algorithm_parameters$roost$x, algorithm_parameters$roost$y, algorithm_parameters$roost$radius)
 
     suppressWarnings({
-        dtm <- read_db_raster("dtm", ext, database_host, database_name, database_port, database_user, database_password)
-        dsm <- read_db_raster("dsm", ext, database_host, database_name, database_port, database_user, database_password)
-        lcm <- read_db_raster("lcm", ext, database_host, database_name, database_port, database_user, database_password)
+        dtm <- read_db_raster(dtm_table, ext, database_host, database_name, database_port, database_user, database_password)
+        dsm <- read_db_raster(dsm_table, ext, database_host, database_name, database_port, database_user, database_password)
+        lcm <- read_db_raster(lcm_table, ext, database_host, database_name, database_port, database_user, database_password)
 
         expect_gt(length(dtm), 0)
         expect_gt(length(dsm), 0)
