8041fb8a53d13cae12dab452c4a2b550e7204f9d
j <you@example.com>
2022-08-10 12:34:24 +0100

    refactor and docs for private

--- circuitscape_app/drawing_collection.R
+++ circuitscape_app/drawing_collection.R
@@ -8,123 +8,73 @@ source("circuitscape_app/drawing.R")
 source("R/transform.R")
 
 
-remove_height_param <- function(i) {
-    logger::log_info("Removing height param")
-    removeUI(selector=paste0("div:has(> #HEIGHT", i, ")"), immediate = TRUE)
-}
-
-#' Transform drawings to correct coordinates for existing pipeline
-transform_extra_geoms_27700 <- function(spdata) {
-
-    logger::log_debug("Trying to get extra geoms now")
-    data <- spdata$shapes
-
-
-    if (length(spdata$shapes$buildings) > 0) {
-        logger::log_debug("Attempting to get extra buildings")
-        extra_buildings_t <- sp::spTransform(data$buildings, "+init=epsg:27700")
-        spdata_t$shapes$buildings <- extra_buildings_t
-    }
-
-    extra_roads <- data$roads
-    extra_roads_t <- NULL
-
-    # TODO: DRY
-    if (length(extra_roads) > 0) {
-        logger::log_debug("Attempting to get extra roads")
-        logger::log_debug(extra_roads)
-        terra::crs(extra_roads) <- sp::CRS("+init=epsg:4326")
-        logger::log_debug(extra_roads)
-        extra_roads_t <- sp::spTransform(extra_roads, "+init=epsg:27700")
-        logger::log_debug(extra_roads_t)
-        spdata_t$shapes$roads <- extra_roads_t
-    }
-
-    extra_rivers <- data$rivers
-    extra_rivers_t <- NULL
-
-    if (length(extra_rivers) > 0) {
-        logger::log_debug("Attempting to get extra rivers")
-        logger::log_debug(extra_rivers)
-        terra::crs(extra_rivers) <- sp::CRS("+init=epsg:4326")
-        logger::log_debug(extra_rivers)
-        extra_rivers_t <- sp::spTransform(extra_rivers, "+init=epsg:27700")
-        logger::log_debug(extra_rivers_t)
-        spdata_t$shapes$rivers <- extra_rivers_t
-    }
-
-    logger::log_debug("getting extra lights")
-    extra_lights <- data$lights
-    if (length(extra_lights) > 0) {
-        eldf <- data.frame(x=extra_lights$x, y=extra_lights$y, z=extra_lights$z)
-        converted_pts <- vector_convert_points(eldf, 4326, 27700)
-        extra_lights_t <- converted_pts
-        extra_lights_t$z <- unlist(spdata$z$lights)
-    } else {
-        extra_lights_t <- data.frame(x=c(), y=c(), z=c())
-    }
-
-    spdata_t$shapes$lights <- extra_lights_t
-
-    logger::log_debug("Returning extras")
-    return(spdata_t)
-}
-
-#' A widget allowing the user to draw shapes on a map
+#' Remove the UI height param element i
 #'
-#' This class, when initialized with a session, input, and map reference,
+#' @param i and integer index
+remove_height_param <- function(i) {
+    logger::log_info("Removing height param...")
+    shiny::removeUI(selector = paste0("div:has(> #HEIGHT", i, ")"), immediate = TRUE)
+}
+
+#' A DrawingCollection widget allowing the user to draw shapes on a map
+#'
+#' @description This class, when initialized with a session, input, and map reference,
 #' will insert UI elements, observers, etc. to allow drawing by the user.
 #' This widget is designed to be used within a bsCollapse.
 #'
 #' The widget maintains a 'selected' drawing; the one that is selected at any time
 #' is the one that is modified by the user.
 #'
-#' @docType class
-#' @importFrom R6 R6Class
-#' @export
-#' @keywords points, map
-#' @return Object of \code{\link{R6Class}}
-#' @format \code{\link{R6Class}} object.
+#' @field n Number of vertices.
+#' @field 
 #' @examples
 #' dc <- DrawingCollection$new(input, session, leafletProxy("map"))
-#' @field n Number of vertices.
+#' @export
 DrawingCollection <- R6Class("DrawingCollection",
+
     private = list(
 
+        # TODO: enforce limits more sensibly
         MAX_DRAWINGS = 1000,
+
+        # List of drawings
         drawings = list(),
+
+        # Lists of observers; when a drawing is deleted, observers are cleaned up too
         oi_selectors = list(),
         oi_collapses = list(),
         oi_sliders = list(),
         oi_deletors = list(),
         oi_eyes = list(),
+
+        # Map, session, and input references
         map_proxy = NULL,
         session = NULL,
         input = NULL,
 
+        # Counters
+        n_created = 0,
+        n_drawings = 0,
+        selected_i = -1,
+
+        # UI selector, after which the widget elements are inserted
         ui_selector = NULL,
 
+        # TODO: check if this is of any use at all
+        # A switch to block the check box observer
         block_check_obs = FALSE,
 
+        #' Create observer for added shape
         create_add_obs = function() {
 
-            o <- observeEvent(private$input$add_drawing, {
+            o <- shiny::observeEvent(private$input$add_drawing, {
                 logger::log_info("Adding drawing...")
                 self$create_new_drawing("building")
             })
 
         },
 
-        trigger_type_update = function(i, new_type) {
-
-            pretty_types = list(building="Building", river="River", lights="Lights", lightstring="Light String", road="Road")
-            new_type = pretty_types[[new_type]]
-
-            selectname <- get_selectname(i)
-            logger::log_info(paste("Triggering update to select input", selectname, new_type))
-            updateSelectInput(private$session, selectname, selected=new_type)
-        },
-
+        #' Change the type of drawing i to new_type
         change_type = function(i, new_type) {
 
             logger::log_debug(paste("Changing type...", i, new_type))
@@ -181,16 +131,19 @@ DrawingCollection <- R6Class("DrawingCollection",
             textname <- get_textname(i)
             eyename <- get_eyename(i)
 
-            private$oi_selectors[[i]] <- observeEvent(private$input[[selectname]], {
+            # Create type selector
+            private$oi_selectors[[i]] <- shiny::observeEvent(private$input[[selectname]], {
                 new_type <- gsub(" ", "", tolower(private$input[[selectname]]), fixed=TRUE)
                 logger::log_info(paste("Selector triggered for drawing with new type", new_type))
                 dr <- private$drawings[[as.character(i)]]
                 dr$clear_graphics(private$map_proxy)
                 private$change_type(i, new_type)
             }, ignoreInit = TRUE)
+
             logger::log_info("Created selector...")
 
-            private$oi_collapses[[i]] <- observeEvent(private$input[[checkname]], {
+            # Create check box selection
+            private$oi_collapses[[i]] <- shiny::observeEvent(private$input[[checkname]], {
                 logger::log_info(paste("Check triggered for drawing", i))
 
                 if (private$block_check_obs) {
@@ -202,35 +155,40 @@ DrawingCollection <- R6Class("DrawingCollection",
                 if (private$input[[checkname]]) {
                     logger::log_info(paste("Check triggered with input state", private$input[[checkname]]))
                     self$select(i)
-                    # self$selected_i <- i
+                    # private$selected_i <- i
                 } else {
                     self$unselect_no_ui_update(i)
                 }
             }, ignoreInit = TRUE)
+
             logger::log_info("Created Check...")
 
-            private$oi_sliders[[i]] <- observeEvent(private$input[[paste0("HEIGHT", i)]], {
+            # Create height slider observer; may be removed again
+            private$oi_sliders[[i]] <- shiny::observeEvent(private$input[[paste0("HEIGHT", i)]], {
                 private$drawings[[as.character(i)]]$height <- as.double(private$input[[paste0("HEIGHT", i)]])
             })
+
             logger::log_info("Created slider...")
 
-            private$oi_deletors[[i]] <- observeEvent(private$input[[buttonname]], {
+            # Create drawing deletion button observer
+            private$oi_deletors[[i]] <- shiny::observeEvent(private$input[[buttonname]], {
                 self$delete(divname, i)
             }, ignoreInit = TRUE, once = TRUE)
+
             logger::log_info("Created delete button...")
 
-            private$oi_eyes[[i]] <- observeEvent(private$input[[eyename]], {
+            # Create eye observer to hide drawings
+            private$oi_eyes[[i]] <- shiny::observeEvent(private$input[[eyename]], {
                 logger::log_info("Triggering eye observer...")
                 private$drawings[[as.character(i)]]$set_visibility(private$map_proxy, private$input[[eyename]])
             }, ignoreInit = TRUE)
+
             logger::log_info("Created eye button...")
 
         }
     ),
-    public= list(
-        selected_i = -1,
-        n_created = 0,
-        n_drawings = 0,
+
+    public = list(
 
         initialize = function(input, session, map, ui_selector = '#drawing_collection_ui') {
             logger::log_info("Initializing drawing collection...")
@@ -246,39 +204,31 @@ DrawingCollection <- R6Class("DrawingCollection",
 
         },
 
-        # #' Select without updating UI components
-        # #'  Needed because updating UI will trigger selection again... infinite loop
-        # select_no_update = function(i, box_is_checked) {
-        #     logger::log_info(paste("Selecting no update", i))
-        #     self$selected_i <- i
-        #     updateCheckboxInput(private$session, get_checkname(i), value = 1)
-        # },
-
         select = function(i, box_is_checked) {
             logger::log_info(paste("Selecting", i))
             private$block_check_obs <- TRUE
             self$unselect()
-            # self$uncheck(self$selected_i)
-            self$selected_i <- i
+            # self$uncheck(private$selected_i)
+            private$selected_i <- i
             updateCheckboxInput(private$session, get_checkname(i), value = 1)
             private$block_check_obs <- FALSE
         },
 
         delete = function(divname, i) {
             logger::log_info("Deleting object")
-            self$n_drawings <- self$n_drawings - 1
+            private$n_drawings <- private$n_drawings - 1
             removeUI(selector = paste0("#", divname))
             private$drawings[[as.character(i)]]$clear_graphics(private$map_proxy)
             private$drawings[[as.character(i)]] <- NULL
-            if (is.null(self$selected_i) || self$selected_i == i) {
-                self$selected_i <- NULL
+            if (is.null(private$selected_i) || private$selected_i == i) {
+                private$selected_i <- NULL
             }
             private$oi_selectors[[i]]$destroy()
             private$oi_collapses[[i]]$destroy()
             private$oi_sliders[[i]]$destroy()
             private$oi_deletors[[i]]$destroy()
             private$oi_eyes[[i]]$destroy()
-            if (self$n_drawings < private$MAX_DRAWINGS) {
+            if (private$n_drawings < private$MAX_DRAWINGS) {
                 enable("add_drawing")
             }
         },
@@ -536,7 +486,7 @@ DrawingCollection <- R6Class("DrawingCollection",
             logger::log_info("Adding points")
 
             if (is.null(j)) {
-                j <- self$selected_i
+                j <- private$selected_i
             }
 
             for (i in seq_along(xs)) {
@@ -551,7 +501,7 @@ DrawingCollection <- R6Class("DrawingCollection",
         add_point_complete = function(x, y, j=NULL) {
 
             if (is.null(j)) {
-                j <- self$selected_i
+                j <- private$selected_i
             }
 
             if (is.null(j) || private$drawings[[as.character(j)]]$is_complete) {
@@ -574,16 +524,16 @@ DrawingCollection <- R6Class("DrawingCollection",
         },
 
         get_selected_drawing = function() {
-            dr <- private$drawings[[self$selected_i]]
+            dr <- private$drawings[[private$selected_i]]
             return(dr)
         },
 
         something_is_selected = function() {
-            logger::log_info(paste("Something is selected", self$selected_i))
-            if (is.null(self$selected_i)) {
+            logger::log_info(paste("Something is selected", private$selected_i))
+            if (is.null(private$selected_i)) {
                 return(FALSE)
             }
-            if (self$selected_i > 0) {
+            if (private$selected_i > 0) {
                 return(TRUE)
             }
             return(FALSE)
@@ -604,41 +554,41 @@ DrawingCollection <- R6Class("DrawingCollection",
 
         #' unselect
         unselect = function() {
-            logger::log_info(paste("Unselecting", self$selected_i))
-            if (!is.null(self$selected_i)) {
+            logger::log_info(paste("Unselecting", private$selected_i))
+            if (!is.null(private$selected_i)) {
                 logger::log_info(paste("Unchecking box and setting to null"))
-                self$uncheck_box(self$selected_i)
-                self$selected_i <- NULL
+                self$uncheck_box(private$selected_i)
+                private$selected_i <- NULL
             } else {
                 logger::log_info(paste("Unselect did nothing"))
             }
-            logger::log_info(paste("Selected is now", self$selected_i))
+            logger::log_info(paste("Selected is now", private$selected_i))
         },
 
         unselect_no_ui_update = function(k) {
             logger::log_info("Unselecting but not updating ui")
             # Only act if k is currently selected
-            if (is.null(self$selected_i)) {
+            if (is.null(private$selected_i)) {
                 logger::log_info("Nothing is currently selected")
                 return()
             }
-            if (self$selected_i != k) {
+            if (private$selected_i != k) {
                 logger::log_info(paste(k, "is not currently selected"))
                 return()
             }
-            self$selected_i <- NULL
+            private$selected_i <- NULL
         },
 
         #' height can be a vector for lights
         create_new_drawing = function(type, height=10, label=NULL) {
 
             logger::log_info("Attempting to create a new object")
-            if (self$n_drawings < private$MAX_DRAWINGS) {
+            if (private$n_drawings < private$MAX_DRAWINGS) {
 
-                self$n_created <- self$n_created + 1
-                self$n_drawings <- self$n_drawings + 1
+                private$n_created <- private$n_created + 1
+                private$n_drawings <- private$n_drawings + 1
 
-                di <- self$n_created
+                di <- private$n_created
 
                 if (type == "building") {
                     dr <- DrawnPolygon$new(di, "building", height)
@@ -672,7 +622,7 @@ DrawingCollection <- R6Class("DrawingCollection",
 
                 private$create_observers(di)
 
-                if (self$n_drawings == private$MAX_DRAWINGS) {
+                if (private$n_drawings == private$MAX_DRAWINGS) {
                     disable("add_drawing")
                 }
 
