c6b55a585b7331898873d28371ae461fbbee9164
Alex Southgate <joelsouthgate@googlemail.com>
2026-09-10 11:17:06 +0100

    speed up docker (#49)

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,23 +1,9 @@
-docker build -f docker/Dockerfile.frontend.builder -t frontend-builder:latest .
+#!/bin/bash
+set -euo pipefail
+export DOCKER_BUILDKIT=1
 
-docker run --rm \
-  -v "$(pwd)":/app \
-  -w /app/frontend/wasm-connectivity \
-  frontend-builder:latest \
-  make build
+FLAGS=("$@")
 
-docker run --rm \
-  -v "$(pwd)":/app \
-  -w /app/frontend/gsbio-engine \
-  frontend-builder:latest \
-  sh -c "npm install && npm run build"
-
-docker run --rm \
-  -v "$(pwd)":/app \
-  -w /app/frontend \
-  frontend-builder:latest \
-  sh -c "npm install && npm run build"
-
-docker build --network=host -t dispersion-prediction-app-frontend -f docker/Dockerfile.frontend . --build-context gsbio=./frontend/gsbio-engine
-docker build -t dispersion-prediction-app-api -f docker/Dockerfile.backend .
-docker build -t dispersion-prediction-app-postgis -f docker/Dockerfile.gis .
+docker build "${FLAGS[@]}" -t dispersion-prediction-app-frontend -f docker/Dockerfile.frontend .
+docker build "${FLAGS[@]}" -t dispersion-prediction-app-api      -f docker/Dockerfile.backend .
+docker build "${FLAGS[@]}" -t dispersion-prediction-app-postgis  -f docker/Dockerfile.gis .
